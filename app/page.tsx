2c0ddc40c76ba1519006662c66d7859c11c431fd
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2025-04-09 21:47:51 +0900

    @supabase/auth-helpers-nextjs をインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+
+import { createServerComponentClient } from "http";
+
 
 export default function Home() {
+
+  const supabase = 
+
+
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+
+      
+
+
         <div>Hi, My name is Kazuki ! </div>
       </main>
     </div>
